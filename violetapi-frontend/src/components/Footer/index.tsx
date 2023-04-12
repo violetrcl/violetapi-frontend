feacf8b14a684a7cbc60c33f449baebf642fc3c4
violetRcl <108343476+violetrcl@users.noreply.github.com>
2023-04-12 17:31:33 +0800

    API开放平台前端
    基于ant design pro开发的API开放平台前端

---
+++ violetapi-frontend/src/components/Footer/index.tsx
@@ -0,0 +1,36 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+import '@umijs/max';
+const Footer: React.FC = () => {
+  const defaultMessage = 'violetRcl出品';
+  const currentYear = new Date().getFullYear();
+  return (
+    <DefaultFooter
+      style={{
+        background: 'none',
+      }}
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        {
+          key: 'violet接口',
+          title: 'violet接口',
+          href: 'https://pro.ant.design',
+          blankTarget: true,
+        },
+        {
+          key: 'github',
+          title: <GithubOutlined />,
+          href: 'https://github.com/ant-design/ant-design-pro',
+          blankTarget: true,
+        },
+        {
+          key: 'violet接口',
+          title: 'violet接口',
+          href: 'https://ant.design',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
+export default Footer;
